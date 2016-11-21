0e8dedd23ed2384efc6d51a0e75726dd87117d32
Miroslav Hudak <mhudak@dev.enscope.com>
2016-11-21 23:35:50 +0100

    Added contributions to license text

--- build/jquery.mapify.js
+++ build/jquery.mapify.js
@@ -3,6 +3,7 @@
 The MIT License (MIT)
 
 Copyright (c) 2014 etienne-martin
+Contributions by Miro Hudak <mhudak@dev.enscope.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
